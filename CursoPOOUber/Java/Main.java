88645fe22fdb01de1136527fdbdac0a5e06ed21b
JairHero <88945281+JairHero@users.noreply.github.com>
2022-10-05 19:51:51 -0500

    Add files via upload

---
+++ CursoPOOUber/Java/Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, world");
+
+        Car car = new Car();
+        car.licence = "A2";
+        car.driver = "jair hernandez";
+        car.passegenger = 4;
+        
+        Car car1 = new Car();
+        car1.licence = "01 A2";
+        car1.driver = "01 jair hernandez";
+        car1.passegenger = 01 4;
+    }
+}
